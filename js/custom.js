aaf86e246961f158e6ab13ad9fc3266dc6a84f44
Aquatheme <86432388+Aquatheme@users.noreply.github.com>
2021-06-25 00:39:30 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,77 @@
-/*
-------------------------------------------------------------------------
-* Template Name    : Brelim | Responsive Bootstrap 4 Landing Template * 
-* Author           : AquaTheme                                       *
-* Version          : 1.0.0                                            *
-* Created          : June-2021                                        *
-* File Description : Main Js file of the template                     *
-*-----------------------------------------------------------------------
-*/
+/* Cycle v1.0 by AquaTheme */
+/* TABLE OF CONTENTS */
+/* ------------------
+1. Header script
+2. Carousel script
+3. Active class to toggle script 
+4. Show plans script 
+-------------------- */
 
-$(window).on('scroll', function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 50) {
-        $(".sticky").addClass("stickyadd");
+// Header script
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+        $('#navbar').addClass('header-scrolled');
     } else {
-        $(".sticky").removeClass("stickyadd");
+        $('#navbar').removeClass('header-scrolled');
     }
 });
 
-$('.nav-item a, .scroll_down a').on('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - 0
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
+
+
+
+// Carousel script
+
+$('.testimonials-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
+
+
+// Active class to toggle script 
+
+var $links = $('.pricing-btn');
+$links.click(function() {
+    $links.removeClass('active');
+    $(this).addClass('active');
 });
 
-$("#navbarCollapse").scrollspy({
-    offset: 70
+
+
+// Show plans script 
+
+$(".monthly-plan").hide();
+
+$("#yearly").click(function() {
+    $(".monthly-plan").hide();
+    $(".yearly-plan").show();
 });
 
-$('.video_play').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
+$("#monthly").click(function() {
+    $(".monthly-plan").show();
+    $(".yearly-plan").hide();
 });
 
+
+// Back To Top 
 $(window).on('scroll', function() {
-    if ($(this).scrollTop() > 100) {
-        $('.back_top').fadeIn();
+    if ($(this).scrollTop() < 100) {
+        $('.back_top').slideUp();
     } else {
-        $('.back_top').fadeOut();
+        $('.back_top').slideDown();
     }
 });
 $('.back_top').on('click', function() {
@@ -52,210 +80,3 @@ $('.back_top').on('click', function() {
     }, 1000);
     return false;
 });
-
-
-function basicmap() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 11,
-        scrollwheel: false,
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#e9e9e9"
-                    },
-                    {
-                        "lightness": 17
-                    }
-                ]
-            },
-            {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f5f5f5"
-                    },
-                    {
-                        "lightness": 20
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 17
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 29
-                    },
-                    {
-                        "weight": 0.2
-                    }
-                ]
-            },
-            {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 18
-                    }
-                ]
-            },
-            {
-                "featureType": "road.local",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 16
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f5f5f5"
-                    },
-                    {
-                        "lightness": 21
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#dedede"
-                    },
-                    {
-                        "lightness": 21
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 16
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "saturation": 36
-                    },
-                    {
-                        "color": "#333333"
-                    },
-                    {
-                        "lightness": 40
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f2f2f2"
-                    },
-                    {
-                        "lightness": 19
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#fefefe"
-                    },
-                    {
-                        "lightness": 20
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "color": "#fefefe"
-                    },
-                    {
-                        "lightness": 17
-                    },
-                    {
-                        "weight": 1.2
-                    }
-                ]
-            }
-        ]
-        
-    };
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('contact-map');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        title: 'Cryptox'
-    });
-}
-if ($('#contact-map').length != 0) {
-    google.maps.event.addDomListener(window, 'load', basicmap);
-}
